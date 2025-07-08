352b142f36b9086374eb6736645d691cb1ff237c
M <agence.locale@gmail.com>
2025-07-08 09:20:35 +0200

    load image button

--- main.js
+++ main.js
@@ -1,7 +1,15 @@
-const { app, BrowserWindow, Menu } = require('electron');
+const { app, BrowserWindow, dialog, ipcMain, Menu } = require('electron');
 const path = require('path');
 
 Menu.setApplicationMenu(null);
+
+ipcMain.handle('show-open-dialog', async (event, { title, exts }) => {
+  const result = await dialog.showOpenDialog({
+    properties: ['openFile'],
+    filters: [{ name: title, extensions: exts }]
+  });
+  return result;
+});
 
 function createWindow() {
   const win = new BrowserWindow({
--- preload.js
+++ preload.js
@@ -1,9 +1,11 @@
 const fs = require('fs');
 const path = require('path');
-const { contextBridge } = require('electron');
+const { contextBridge, ipcRenderer   } = require('electron');
 
 contextBridge.exposeInMainWorld('myAPI', {
   readFile: (filePath, options) => fs.readFile(filePath, options),
   joinPath: (...args) => path.join(...args),
   dirname: () => __dirname,
+  showOpenDialog: (title, exts) => 
+    ipcRenderer.invoke('show-open-dialog', { title, exts })
 });
