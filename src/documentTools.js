c23370eeba64555d0ab14c37cf0655a98a72957a
M <agence.locale@gmail.com>
2025-07-09 17:22:31 +0200

    move out fetchUrlOverride implementation

--- src/documentTools.js
+++ src/documentTools.js
@@ -31,4 +31,57 @@ export class DocumentTools
         input.click();
         });
     }
+
+    // Override fetch globally to workaround root file loading issues in electron
+    // or force local loading instead of remote loading
+    static fetchUrlOverride(handler)
+    {
+      if (typeof myAPI !== 'undefined')
+      {
+        const originalFetch = window.fetch;
+        window.fetch = async (url) => {
+          let overridePath = null;
+          if (typeof url === "string")
+          {
+            const override = handler(url);
+            if (override)
+            {
+              overridePath = override.overridePath;
+              url = override.url;
+            }
+          };
+          if (overridePath !== null)
+          {
+            console.log(`Intercepted fetch request for: ${url}`);
+            const basePath = myAPI.joinPath(myAPI.dirname(), overridePath);
+            const fullPath = myAPI.joinPath(basePath, url);
+            console.log(`Path resolved to: ${fullPath}`);
+
+            return new Promise((resolve, reject) => {
+              myAPI.readFile(fullPath, (err, data) => {
+                if (err) {
+                  resolve(new Response(null, {
+                    status: 404,
+                    statusText: "File Not Found"
+                  }));
+                }
+                else
+                {
+                  // Warning: only js, wasm and binary files support is required for piper-tts-web requests
+                  resolve(new Response(data, {
+                    status: 200,
+                    statusText: "OK",
+                    headers: { "Content-Type": url.endsWith(".js")?"application/javascript" :
+                        url.endsWith(".wasm")?"application/wasm":
+                        "application/octet-stream"
+                    }
+                  }));
+                }
+              });
+            });
+          }
+          return originalFetch(url);
+        };
+      }
+    }
 }
